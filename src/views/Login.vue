c03aaefddf52c54f64d48a5896dec3d5231eb067
Tristan Robert <tristan.robert.44@gmail.com>
2018-11-16 11:32:48 +0100

    Fix #2: formulaire authentification

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -4,19 +4,43 @@ v-app
     v-container.fluid.fill-height
       v-layout.align-center.justify-center.wrap
         v-flex.xs12.sm8.md4.text-xs-center
-          v-form(@submit.prevent="login()")
             v-card.elevation-12
               v-card-text
                 h1.mb-3 Connexion à Fil'Harmonic
                 p TODO connexion Cerbère / France Connect
-              v-card-actions
-                v-btn(block type="submit") Se connecter
+                v-form(@submit.prevent="login()")
+                  v-text-field(
+                    prepend-icon="person"
+                    name="login"
+                    label="Login"
+                    required)
+                  v-text-field(
+                    prepend-icon="lock"
+                    :append-icon="show ? 'visibility_off' : 'visibility'"
+                    :type="show ? 'text' : 'password'"
+                    @click:append="show = !show"
+                    name="password"
+                    label="Mot de passe"
+                    required)
+                  v-card-actions
+                    v-btn(block type="submit") Se connecter
 
-          v-btn.mt-4(to="/" flat small) Retourner à la page d'accueil
+            v-btn.mt-4(to="/" flat small) Retourner à la page d'accueil
 </template>
 
 <script>
 export default {
+  data () {
+    return {
+      show: false,
+      password: 'Mot de passe',
+      rules: {
+        required: value => !!value || 'Requis.',
+        min: v => v.length >= 8 || 'Min 8 caractères',
+        emailMatch: () => ('Le login et le mot de passe ne correspondent pas.')
+      }
+    }
+  },
   methods: {
     login () {
       console.log('TODO login')
