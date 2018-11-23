a84cde62574d6f9e5d2fb92cbcc9ec0f797282c7
Maxime Dréau <maxime@dreau.fr>
2018-11-23 10:07:08 +0100

    ajout d'un paramètre par défaut pour util.slow

--- src/util/index.js
+++ src/util/index.js
@@ -2,7 +2,7 @@ export function sleep (ms) {
   return new Promise(resolve => setTimeout(resolve, ms))
 }
 
-export function slow (func, ms) {
+export function slow (func, ms = 100) {
   return async (...args) => {
     await sleep(ms)
     return func(...args)
