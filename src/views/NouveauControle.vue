9c899f8b87f4e8fe1442add0601a103137a7671a
Tristan Robert <tristan.robert.44@gmail.com>
2018-11-23 16:57:16 +0100

    Utilisation composant detail etablissement dans nouveau controle

--- src/views/NouveauControle.vue
+++ src/views/NouveauControle.vue
@@ -3,30 +3,7 @@ v-container.grid-list-lg.controle-form
   v-alert.text-xs-center(type="error" :value="error") L'établissement '#[strong {{ this.$route.params.id }}]' ne semble pas exister. Mauvaise URL ?
   div(v-if="controle.etablissement")
     h1.display-2 Nouveau contrôle
-
-    v-card.mt-4
-      v-toolbar(flat)
-        v-toolbar-title Etablissement {{ controle.etablissement.nom }} n°{{ controle.etablissement.id }}
-      v-card-text
-        v-container.pa-0.grid-list-sm
-          v-layout
-            v-flex S3IC
-            v-flex.text-xs-right {{ controle.etablissement.id }}
-          v-layout
-            v-flex Nom usuel
-            v-flex.text-xs-right {{ controle.etablissement.nom }}
-          v-layout
-            v-flex Raison sociale
-            v-flex.text-xs-right {{ controle.etablissement.raison }}
-          v-layout
-            v-flex Adresse
-            v-flex.text-xs-right {{ controle.etablissement.adresse }}
-          v-layout
-            v-flex Activité principale
-            v-flex.text-xs-right {{ controle.etablissement.activite }}
-          v-layout
-            v-flex Régime Seveso
-            v-flex.text-xs-right Haut
+    fh-detail-etablissement(v-if="!error", :etablissement="controle.etablissement")
 
     h4.display-1.mt-4 Détails du contrôle
 
@@ -39,10 +16,12 @@ v-container.grid-list-lg.controle-form
 import FhDetailControle from '@/components/FhDetailControle.vue'
 import { createControle } from '@/api/controles'
 import { getEtablissement } from '@/api/etablissements'
+import FhDetailEtablissement from '@/components/FhDetailEtablissement.vue'
 
 export default {
   components: {
-    FhDetailControle
+    FhDetailControle,
+    FhDetailEtablissement
   },
   data () {
     return {
