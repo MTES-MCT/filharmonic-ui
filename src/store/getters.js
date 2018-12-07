bde38ec7f886e00210e6785471336b83f8419046
Maxime Dréau <maxime@dreau.fr>
2018-12-07 14:13:09 +0100

    store: vérification que l'utilisateur est valide pour les getters permissions

--- src/store/getters.js
+++ src/store/getters.js
@@ -1,12 +1,12 @@
 export default {
   // permissions
   isInspecteur (state) {
-    return state.authentication.user.type === 'inspecteur'
+    return state.authentication.valid && state.authentication.user.type === 'inspecteur'
   },
   isApprobateur (state) {
-    return state.authentication.user.type === 'approbateur'
+    return state.authentication.valid && state.authentication.user.type === 'approbateur'
   },
   isExploitant (state) {
-    return state.authentication.user.type === 'exploitant'
+    return state.authentication.valid && state.authentication.user.type === 'exploitant'
   }
 }
