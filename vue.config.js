d61d1163997eabb8c25a9290309ad78d3d0af494
Tristan Robert <tristan.robert.44@gmail.com>
2019-02-06 16:56:31 +0100

    Fix e2e dev server tests

--- vue.config.js
+++ vue.config.js
@@ -2,14 +2,20 @@ module.exports = {
   productionSourceMap: false,
   devServer: {
     disableHostCheck: true,
-    host: 'filharmonic.local.beta.gouv.fr',
+    host: '0.0.0.0',
     proxy: {
       '/api': {
         target: 'http://localhost:5000',
         pathRewrite: {
           '^/api': ''
-        }
+        },
+        public: 'filharmonic.local.beta.gouv.fr:8080'
       }
+    },
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE",
+      "Access-Control-Allow-Headers": "X-Requested-With, content-type, authorization"
     }
   }
 }
