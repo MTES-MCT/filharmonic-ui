5ed45c911edde6263768ec8416649afe0e4814f2
Maxime Dréau <maxime@dreau.fr>
2019-02-11 17:57:01 +0100

    Lint

--- vue.config.js
+++ vue.config.js
@@ -13,9 +13,9 @@ module.exports = {
       }
     },
     headers: {
-      "Access-Control-Allow-Origin": "*",
-      "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE",
-      "Access-Control-Allow-Headers": "X-Requested-With, content-type, authorization"
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE',
+      'Access-Control-Allow-Headers': 'X-Requested-With, content-type, authorization'
     }
   }
 }
